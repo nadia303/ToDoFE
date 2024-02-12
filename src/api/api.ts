c434f55815c735d42951a5ccad7829eb4bfa677d
Nadiia Mishchuk <n.mishchuk@apiko.com>
2024-02-12 21:52:11 +0200

    change apiUrl for backend

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-export const apiUrl = process.env.REACT_APP_API_URL
+// export const apiUrl = process.env.REACT_APP_API_URL
+
+export const apiUrl = 'http://todobe-production-2979.up.railway.app'
 
 export const apiConfig = {
   baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    // withAuth: true, // Adding Authorization header by default
   },
 }
 export const api = axios.create(apiConfig)
